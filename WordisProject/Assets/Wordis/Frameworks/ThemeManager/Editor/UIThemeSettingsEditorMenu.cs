7f51d9d89a190c442091c28e35c9d35ac16d58bd
NicklausBrain <nikolay.mozgovoy@sigma.software>
2021-01-08 18:31:13 +0200

    Hyperbyte -> wordis (#4)
    * HYPERBYTE namespace -> Wordis
    
    * Hyperbyte -> Wordis

---
+++ WordisProject/Assets/Wordis/Frameworks/ThemeManager/Editor/UIThemeSettingsEditorMenu.cs
@@ -0,0 +1,52 @@
+﻿// ©2019 - 2020 HYPERBYTE STUDIOS LLP
+// All rights reserved
+// Redistribution of this software is strictly not allowed.
+// Copy of this software can be obtained from unity asset store only.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Assets.Wordis.Frameworks.ThemeManager.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Wordis.Frameworks.ThemeManager.Editor
+{
+    public class UIThemeSettingsEditorMenu : MonoBehaviour
+    {
+        #region MobileAdsSetting
+        [MenuItem("Wordis/UI Theme Settings", false, 16)]
+        public static void GenerateUIThemeSettingsScriptable()
+        {
+            string assetPath = $"Assets/{nameof(Wordis)}/Resources";
+            string assetName = "UIThemeSettings.asset";
+
+            UIThemeSettings asset;
+
+            if (!System.IO.Directory.Exists(assetPath))
+            {
+                System.IO.Directory.CreateDirectory(assetPath);
+            }
+
+            if (System.IO.File.Exists($"{assetPath}/{assetName}"))
+            {
+                asset = (UIThemeSettings)Resources.Load(System.IO.Path.GetFileNameWithoutExtension(assetName));
+            }
+            else
+            {
+                asset = ScriptableObject.CreateInstance<UIThemeSettings>();
+                AssetDatabase.CreateAsset(asset, $"{assetPath}/{assetName}");
+                AssetDatabase.SaveAssets();
+            }
+
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+        #endregion
+    }
+}
