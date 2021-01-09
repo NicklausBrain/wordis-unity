27cd1aec111f2e8afb5a939a3bfec5d0cdac7eb8
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-01-09 22:01:50 +0200

    first unit test

---
+++ WordisProject/Assets/Wordis/BlockPuzzle/GameCore/Tests/WordisGameTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Assets.Wordis.BlockPuzzle.GameCore.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="WordisGame"/>.
+    /// </summary>
+    public class WordisGameTests
+    {
+        [Test]
+        public void Settings_ReturnsPassedObject()
+        {
+            var settings = new WordisSettings(
+                width: 5,
+                height: 5);
+
+            var game = new WordisGame(settings);
+
+            Assert.AreSame(settings, game.Settings);
+        }
+    }
+}
