3129886ab2872e48801922da5aac7ca515faeb5c
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-01-10 17:19:49 +0200

    HandleLeft_ForActiveChar_MovesItLeft HandleRight_ForActiveChar_MovesItRight

--- WordisProject/Assets/Wordis/BlockPuzzle/GameCore/Tests/WordisGameTests.cs
+++ WordisProject/Assets/Wordis/BlockPuzzle/GameCore/Tests/WordisGameTests.cs
@@ -10,18 +10,6 @@ namespace Assets.Wordis.BlockPuzzle.GameCore.Tests
     public class WordisGameTests
     {
         [Test]
-        public void Settings_ReturnsPassedObject()
-        {
-            var settings = new WordisSettings(
-                width: 5,
-                height: 5);
-
-            var game = new WordisGame(settings);
-
-            Assert.AreSame(settings, game.Settings);
-        }
-
-        [Test]
         public void HandleStep_ForActiveChar_MovesItDown()
         {
             var settings = new WordisSettings(
@@ -31,19 +19,68 @@ namespace Assets.Wordis.BlockPuzzle.GameCore.Tests
             /* Initial state:
              * [-] [W] [-]
              * [-] [-] [-] */
-
-            var game =
-                new WordisGame(settings)
-                    .With(new ActiveChar(1, 1, 'W'))
-                    .Handle(GameEvent.Step);
+            var game = new WordisGame(settings)
+                .With(new ActiveChar(1, 0, 'W'))
+                .Handle(GameEvent.Step);
 
             /* Expected state:
              * [-] [-] [-]
              * [-] [W] [-] */
-
             Assert.AreEqual(
-                new ActiveChar(1, 2, 'W'),
+                new ActiveChar(1, 1, 'W'),
                 game.GameObjects.Single());
+        }
+
+        [Test]
+        public void HandleLeft_ForActiveChar_MovesItLeft()
+        {
+            var settings = new WordisSettings(
+                width: 3,
+                height: 2);
+
+            /* Initial state:
+             * [-] [W] [-] */
+            var game = new WordisGame(settings)
+                .With(new ActiveChar(1, 0, 'W'))
+                .Handle(GameEvent.Left);
+
+            /* Expected state:
+             * [W] [-] [-] */
+            Assert.AreEqual(
+                new ActiveChar(0, 0, 'W'),
+                game.GameObjects.Single());
+        }
+
+        [Test]
+        public void HandleRight_ForActiveChar_MovesItRight()
+        {
+            var settings = new WordisSettings(
+                width: 3,
+                height: 2);
+
+            /* Initial state:
+             * [-] [W] [-] */
+            var game = new WordisGame(settings)
+                .With(new ActiveChar(1, 0, 'W'))
+                .Handle(GameEvent.Right);
+
+            /* Expected state:
+             * [W] [-] [-] */
+            Assert.AreEqual(
+                new ActiveChar(2, 0, 'W'),
+                game.GameObjects.Single());
+        }
+
+        [Test]
+        public void Settings_ReturnsPassedObject()
+        {
+            var settings = new WordisSettings(
+                width: 5,
+                height: 5);
+
+            var game = new WordisGame(settings);
+
+            Assert.AreSame(settings, game.Settings);
         }
     }
 }
