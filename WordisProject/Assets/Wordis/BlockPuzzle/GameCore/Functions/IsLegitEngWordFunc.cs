714a656eb3705fe9e53bb73334909234f6ea629a
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-01-12 22:33:59 +0200

    IsLegitWordFunc IsLegitEngWordFunc

---
+++ WordisProject/Assets/Wordis/BlockPuzzle/GameCore/Functions/IsLegitEngWordFunc.cs
@@ -0,0 +1,18 @@
+﻿using gnuciDictionary;
+
+namespace Assets.Wordis.BlockPuzzle.GameCore.Functions
+{
+    /// <summary>
+    /// Determines if a word is legit in English.
+    /// </summary>
+    public class IsLegitEngWordFunc : IsLegitWordFunc
+    {
+        /// <inheritdoc />
+        public override bool Invoke(string word)
+        {
+            var definition = EnglishDictionary.Define(word);
+
+            return definition != null;
+        }
+    }
+}
