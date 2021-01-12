5b6d3de826b518aafea30b0c458092b1fe2aa77c
NicklausBrain <nikolay.mozgovoy@sigma.software>
2021-01-12 22:48:46 +0200

    Eng dictionary (#10)
    * Nuget unity package
    
    * GNU Collaborative International Dictionary of English
    
    * IsLegitWordFunc
    IsLegitEngWordFunc

---
+++ WordisProject/Assets/Wordis/BlockPuzzle/GameCore/Functions/IsLegitWordFunc.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Wordis.BlockPuzzle.GameCore.Functions
+{
+    /// <summary>
+    /// Determines if a word is a legit word.
+    /// </summary>
+    public abstract class IsLegitWordFunc
+    {
+        /// <summary>
+        /// Invokes the function.
+        /// </summary>
+        /// <param name="word">Target word.</param>
+        /// <returns>True if the word is legit.</returns>
+        public abstract bool Invoke(string word);
+    }
+}
