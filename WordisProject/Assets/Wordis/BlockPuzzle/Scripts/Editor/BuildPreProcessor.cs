5a44e91bbcb0e95c579bb3ef6aba3c2467eb101a
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2021-01-15 00:26:01 +0200

    Added build pre processor with automatically incremented build number.

---
+++ WordisProject/Assets/Wordis/BlockPuzzle/Scripts/Editor/BuildPreProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+public class BuildPreProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder
+    {
+        get { return 0; }
+    }
+    
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        int currentBuildNumber = Int32.Parse(PlayerSettings.iOS.buildNumber);
+        int incrementedBuildNumber = currentBuildNumber + 1;
+        PlayerSettings.iOS.buildNumber = incrementedBuildNumber.ToString();
+        
+        Debug.Log(string.Format("Build number was incremented from {0} to {1}", currentBuildNumber,
+            incrementedBuildNumber));
+    }
+}
