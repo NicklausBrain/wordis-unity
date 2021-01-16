b27a629e16afa2c7c47948c63d393d4e05cb632d
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-01-16 21:50:22 +0200

    decent swipe implementation

--- WordisProject/Assets/Wordis/BlockPuzzle/Scripts/GamePlay/GamingSwipesController.cs
+++ WordisProject/Assets/Wordis/BlockPuzzle/Scripts/GamePlay/GamingSwipesController.cs
@@ -5,38 +5,24 @@ namespace Assets.Wordis.BlockPuzzle.Scripts.GamePlay
 {
     public class GamingSwipesController : MonoBehaviour
     {
-        private Vector2 startTouchPosition;
-        private Vector2 endTouchPosition;
-
         private void Update()
         {
-            if (Input.touchCount > 0 &&
-                Input.GetTouch(0).phase == TouchPhase.Began)
+            if (SwipeManager.IsSwipingLeft())
             {
-                startTouchPosition = Input.GetTouch(0).position;
+                GamePlayUI.Instance.HandleGameEvent(GameEvent.Left);
+                Debug.Log("LEFT SWIPE");
             }
 
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            if (SwipeManager.IsSwipingRight())
             {
-                endTouchPosition = Input.GetTouch(0).position;
+                GamePlayUI.Instance.HandleGameEvent(GameEvent.Right);
+                Debug.Log("RIGHT SWIPE");
+            }
 
-                if (endTouchPosition.x < startTouchPosition.x)
-                {
-                    GamePlayUI.Instance.HandleGameEvent(GameEvent.Left);
-                    Debug.Log("LEFT SWIPE");
-                }
-
-                if (endTouchPosition.x > startTouchPosition.x)
-                {
-                    GamePlayUI.Instance.HandleGameEvent(GameEvent.Right);
-                    Debug.Log("RIGHT SWIPE");
-                }
-
-                if (endTouchPosition.y < startTouchPosition.y)
-                {
-                    GamePlayUI.Instance.HandleGameEvent(GameEvent.Down);
-                    Debug.Log("DOWN SWIPE");
-                }
+            if (SwipeManager.IsSwipingDown())
+            {
+                GamePlayUI.Instance.HandleGameEvent(GameEvent.Down);
+                Debug.Log("DOWN SWIPE");
             }
         }
     }
