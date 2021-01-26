d2ecab2d8dfc187d4f49633769fe1523cf0a8593
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-01-26 12:44:58 +0200

    purified definitions

--- docs/dict/DictPreprocessor/Program.cs
+++ docs/dict/DictPreprocessor/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using gnuciDictionary;
 
 namespace DictPreprocessor
@@ -7,12 +10,54 @@ namespace DictPreprocessor
     {
         static void Main(string[] args)
         {
-            var defns = gnuciDictionary.EnglishDictionary.Define("beaver");
+            var mContent = File.ReadAllText(@"C:\Projects\wordis-unity\docs\dict\!Dictionary-in-csv\M.csv");
 
-            foreach (Word defn in defns)
+            var definitions = GetWordDefinitions(mContent);
+
+            var mapDefinitions = definitions["map"];
+
+            Console.WriteLine(mapDefinitions);
+
+            //Enum.GetNames(typeof(WordCategory)).ToList().ForEach(Console.WriteLine);
+        }
+
+        static Dictionary<string, WordDefinition[]> GetWordDefinitions(string rawContent)
+        {
+            var definitions = rawContent
+                .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(WordDefinition.Parse)
+                .GroupBy(d => d.Word, StringComparer.OrdinalIgnoreCase)
+                .ToDictionary(g => g.Key, g => g.ToArray(), StringComparer.OrdinalIgnoreCase);
+
+            return definitions;
+        }
+
+        public class WordDefinition
+        {
+            public WordDefinition(
+                string word,
+                string category,
+                string definition)
             {
-                Console.WriteLine(defn.Definition);
+                Word = word;
+                Category = category;
+                Definition = definition;
+            }
 
+            public string Word { get; }
+
+            public string Category { get; }
+
+            public string Definition { get; }
+
+            public static WordDefinition Parse(string rawDefinition)
+            {
+                var parts = rawDefinition.Split(new[] { '(', ')' });
+
+                return new WordDefinition(
+                    word: parts[0].Trim(),
+                    category: parts[1].Trim(),
+                    definition: parts[2].Trim());
             }
         }
     }
