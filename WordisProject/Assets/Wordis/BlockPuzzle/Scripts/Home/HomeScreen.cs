2df62ce64e71364b377a519993e7c299915bddf0
Alexander Chipizhenko <alex.chipizhenko@hotmail.com>
2021-01-30 17:52:25 +0200

    - Facebook SDK integration

--- WordisProject/Assets/Wordis/BlockPuzzle/Scripts/Home/HomeScreen.cs
+++ WordisProject/Assets/Wordis/BlockPuzzle/Scripts/Home/HomeScreen.cs
@@ -19,6 +19,7 @@ using Assets.Wordis.Frameworks.InputManager.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
 using GameAnalyticsSDK;
+using Facebook.Unity;
 
 namespace Assets.Wordis.BlockPuzzle.Scripts.Home
 {
@@ -54,6 +55,46 @@ namespace Assets.Wordis.BlockPuzzle.Scripts.Home
 
             gamePlaySettings = null;
             GameAnalytics.Initialize();
+
+            if (!FB.IsInitialized)
+            {
+                // Initialize the Facebook SDK
+                FB.Init(InitCallback, OnHideUnity);
+            }
+            else
+            {
+                // Already initialized, signal an app activation App Event
+                FB.ActivateApp();
+            }
+        }
+
+        private void InitCallback()
+        {
+            if (FB.IsInitialized)
+            {
+                // Signal an app activation App Event
+                FB.ActivateApp();
+                // Continue with Facebook SDK
+                // ...
+            }
+            else
+            {
+                Debug.Log("Failed to Initialize the Facebook SDK");
+            }
+        }
+
+        private void OnHideUnity(bool isGameShown)
+        {
+            if (!isGameShown)
+            {
+                // Pause the game - we will need to hide
+                Time.timeScale = 0;
+            }
+            else
+            {
+                // Resume the game - we're getting focus again
+                Time.timeScale = 1;
+            }
         }
 
         /// <summary>
