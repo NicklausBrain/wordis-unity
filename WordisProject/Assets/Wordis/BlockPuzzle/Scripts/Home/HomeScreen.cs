ee1b8e84c2302c7b3e7423c1a8e61d6954a384a6
Mykola Mozghovyi <nicklausbrain@gmail.com>
2022-01-16 17:09:49 +0200

    - FB (dropped) (#98)

--- WordisProject/Assets/Wordis/BlockPuzzle/Scripts/Home/HomeScreen.cs
+++ WordisProject/Assets/Wordis/BlockPuzzle/Scripts/Home/HomeScreen.cs
@@ -17,7 +17,6 @@ using Assets.Wordis.BlockPuzzle.Scripts.UI.Extensions;
 using Assets.Wordis.Frameworks.InputManager.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
-using Facebook.Unity;
 
 namespace Assets.Wordis.BlockPuzzle.Scripts.Home
 {
@@ -34,31 +33,6 @@ namespace Assets.Wordis.BlockPuzzle.Scripts.Home
         /// </summary>
         void Awake()
         {
-            if (!FB.IsInitialized)
-            {
-                // Initialize the Facebook SDK
-                FB.Init(InitCallback, OnHideUnity);
-            }
-            else
-            {
-                // Already initialized, signal an app activation App Event
-                FB.ActivateApp();
-            }
-        }
-
-        private void InitCallback()
-        {
-            if (FB.IsInitialized)
-            {
-                // Signal an app activation App Event
-                FB.ActivateApp();
-                // Continue with Facebook SDK
-                // ...
-            }
-            else
-            {
-                Debug.Log("Failed to Initialize the Facebook SDK");
-            }
         }
 
         private void OnHideUnity(bool isGameShown)
